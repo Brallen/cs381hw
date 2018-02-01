1dc21e3311ee9334cd9247532e974ce82d44c32d
Brett Case <brett.case97@gmail.com>
2018-02-01 09:03:05 -0800

    adding mostly finished assignment

--- hw2/MiniLogo.casebr.hs
+++ hw2/MiniLogo.casebr.hs
@@ -3,17 +3,11 @@
 import Prelude hiding (Num)
 import Data.List
 
-
---1.Define the abstract syntax of MiniLogo as a set of 
---Haskell data types. You should use built-in types 
---for num, var, and macro. (If you want to define a 
---type Num, you will have to hide that name from the 
---Prelude).
-
 type Num = Int
 type Var = String
 type Macro = String
 
+--1.
 type Prog = [Cmd]
 
 data Mode = Up
@@ -31,12 +25,64 @@ data Cmd = Pen Mode
          | Call Macro [Expr]
          deriving (Show, Eq)
 
---2.Define a MiniLogo macro line (x1,y1,x2,y2) that 
---(starting from anywhere on the canvas) draws a line
---segment from (x1,y1) to (x2,y2).
+--2.
+--Minilogo Macro:
+--define line(x1, y1, x2, y2){
+--    pen up;
+--    move(x1,y1);
+--    pen down;
+--    move(x2,y2);
+--} 
+
 line :: Cmd
 line = Define "line" ["x1", "y1", "x2", "y2"]
-           [Pen Up, Move (Thing "x1", Thing "y1"), Pen Down, Move (Thing "x2", Thing "y2")]
+        [Pen Up, Move (Thing "x1", Thing "y1"), Pen Down, Move (Thing "x2", Thing "y2")]
 
+--3.
+--Minilogo Macro:
+--define nix(x, y, w, h){
+--    line(x, y, x+w, y+h)
+--    line(x, y+h, x+w, y)
+--}
 
+nix :: Cmd
+nix = Define "nix" ["x", "y", "w", "h"]
+        [Call "line" [Thing "x", Thing "y", Add (Thing "x") (Thing "w"), Add (Thing "y") (Thing "h")],
+         Call "line" [Thing "x", Add (Thing "y") (Thing "h"), Add (Thing "x") (Thing "w"), Thing "y"]]
  
+--4.
+--makes steps that start at the top and work down recursively
+
+steps :: Int -> Prog
+steps 0 = []
+steps x = [Call "line" [Number x, Number x, Number (x - 1), Number x], 
+           Call "line" [Number (x-1), Number x, Number (x-1), Number (x-1)]]
+           ++ steps (x-1)
+
+--5.
+
+macros :: Prog -> [Macro]
+macros []     = []
+macros (x:xs) = case x of
+                 Define n _ _ -> n : macros xs
+                 _            -> macros xs
+
+--6.
+
+--takes expressions and turns them into strings
+prettyHelper :: Expr -> String
+prettyHelper (Thing v)  = v
+prettyHelper (Number n) = (show n)
+prettyHelper (Add x y)  = prettyHelper x ++ " + " ++ prettyHelper y
+
+--takes a pen mode and returns a string
+prettyPen :: Mode -> String
+prettyPen x = (show x)
+
+pretty :: Prog -> String
+pretty [] = ""
+pretty ((Pen x):cmds) = "pen " ++ (prettyPen x) ++ ";" ++ (pretty cmds)
+pretty ((Move (x, y)):cmds) = "move" ++ "(" ++ (prettyHelper x) ++ "," ++ (prettyHelper y) ++ ");" ++ (pretty cmds)
+pretty (Define n v p:cmds) = "fill me in" ++ (pretty cmds)--n ++ "(" ++ (intercalate "," v) ++ ")" 
+pretty ((Call m e):cmds) = "fill me in 2" ++ (pretty cmds)--m ++ (prettyHelper e) ++ ";"
+
